921687e62f0983cbcb5a77d9998131a61dc32557
BGCG <billie.griffith4@gmail.com>
2023-08-06 12:51:27 +0000

    Render ML performance plots from outputs folder to streamlit app interface

--- app_pages/ml_performance_page.py
+++ app_pages/ml_performance_page.py
@@ -2,6 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.image import imread
+from src.machine_learning.evaluate_clf import load_test_evaluation
 
 
 def page_ml_performance_metrics():
@@ -13,14 +14,28 @@ def page_ml_performance_metrics():
     st.image(labels_distribution, caption='Labels distrubution on Train, Valdation and Test sets')
     st.write("---")
 
-    col1, col2 = st.beta_columns(2)
-    with col1:
-        model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
-        st.image(model_acc, caption='Model Training accuracy')
-    with col2:
-        model_loss = plt.imread(f"outputs/{version}/model_training_acc.png")
-        st.image(model_loss, caption="Model Training losses")
+    model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
+    st.image(model_acc, caption='Model Training accuracy')
+
+    model_loss = plt.imread(f"outputs/{version}/model_training_acc.png")
+    st.image(model_loss, caption="Model Training losses")
+
+    conf_matrix = plt.imread(f"outputs/{version}/conf_matrix.png")
+    st.image(conf_matrix, caption="Confusion matrix")
+
+    roc_plot = plt.imread(f"outputs/{version}/roc_plot.png")
+    st.image(roc_plot, caption="ROC plot")
+
+    prec_recall_plot = plt.imread(f"outputs/{version}/precision_recall_plot.png")
+    st.image(prec_recall_plot, caption="Precision-recall plot")
+
+    
     st.write("---")
 
     st.write("### Performance on Test Set")
     st.dataframe(pd.DataFrame(load_test_evaluation(version), index=['Loss', 'Accuracy']))
+
+    st.write(
+        f"For additional information, please read this projects readme file\n"
+        f"[Project README file](https://github.com/BGCG/project-5/tree/main)\n"
+    )
