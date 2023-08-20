3f5f39d3c83d7bff06426391c0aecf5e8704a06d
BGCG <>
2023-08-20 11:55:34 +0000

    Add page icon

--- app_pages/multipage.py
+++ app_pages/multipage.py
@@ -9,7 +9,8 @@ class Multipage:
 
         st.set_page_config(
            page_title=self.app_name,
-           page_icon="")
+           page_icon=":seedling:")
+           # short code for page icon from https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
     
     def add_page(self, title, func) -> None:
         self.pages.append({"title": title, "function": func})
