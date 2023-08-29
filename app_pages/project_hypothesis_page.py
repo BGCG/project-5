03c579abdd45065b7aaf32252338979f383a2c37
BGCG <>
2023-08-29 13:17:28 +0000

    Fix PEP8 errors

--- app_pages/project_hypothesis_page.py
+++ app_pages/project_hypothesis_page.py
@@ -14,9 +14,9 @@ def page_project_hypothesis_body():
         f"We suspect that cherry leaves infected with powdery mildew may have "
         f"discernible features that can tell them apart from cherry leaves that are "
         f"not infected."
-        f"Image montage analysis showed that white blemishes were observed "
+        f"* Image montage analysis showed that white blemishes were observed "
         f"on leaves infected with powdery mildew in comparison to those not infected. "
-        f"However, Average Image, Variability image and Difference between images studies did not "
+        f"* However, Average Image, Variability image and Difference between images studies did not "
         f"show any explicit pattern that could be used to differentiate a leaf infected "
         f"with powdery mildew to one that is uninfected."
     )
@@ -24,19 +24,20 @@ def page_project_hypothesis_body():
     st.info(
         f"As the visualisation of the cherry leaves suggested there was no prominent discernable"
         f" features that could tell if a cherry leaf was infected with powdery mildew or not, "
-        f"we decided that a classification tool would be the best route forward to fulfill the client's request. "
+        f"we decided that a classification tool would be the best route forward to fulfill the client's request."
         f"Since the prediction outcome would fall into one of two categories (healthy or powdery mildew infection), "
         f"we decided creation of a binary classification algorithm would be the best approach to build our prediction tool."
     )
 
     st.success(
-        f"We hypothesised usage of the adam optimiser in our model along with performing regularisation techniques such as "
+        f"We hypothesised usage of the adam optimiser in our model along with performing regularisation techniques such as"
         f"image augmentation and internal dropout layers in our model, to improve the models ability to generalise, would result"
-        f" in the model to meet the client's performance goal of 97% accuracy."
-        F"We evaluated our hypotheses by adjusting adjust our hyperparameters (usage of the adam optimiser) and regularisation "
-        f"techniques (introduction of an internal dropout layer) and then performed an evaluation of the adapted model on the test set."
-        f"Our hypothesis was validated as evaluation of the model with the test set yielded a 99.2% accuracy, which met our clients goals. "
-        f"Therefore, in conclusion we were successful in the creation of a machine learning model to predict whether a "
+        f" in the model to meet the clients perfromance goal of 97% accuracy."
+        F"* We evaluted our hypotheses by adjusting adjust our hyperparameters (usage of the adam optimiser) and regularisation "
+        f"techniques (introduction of an internal dropout layer) and then performed an evaluation of the adapted model on the test"
+        f"Our hypothesis was validated as evaluation of the model "
+        f"with the test set yeilded a 99.2% accuracy, which met our clients goals. "
+        f"Therefore, in conclusion we were sucessful in the creation of a machine learning model to predict whether a "
         f" cherry leaf is infected with powdery mildew or not. Further details about the model can be found on the ML "
-        f"performance page of this dashboard."
+        f"perfromance page of this dashboard."
     )
